8c5c4929aeebfaf315c1efed361df0556bdcf41a
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-12 09:23:18 +0200

    Added PostList APIView to handle GET and POST requests for posts

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('profiles.urls')),
+    path('', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
