fa09e380101f2496a1a0c0103204b8d3f3dc057d
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-13 17:07:45 +0200

    Add count fields to Profile model and serializer for posts, followers, and following. Update ordering fields.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,7 +7,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+   
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -23,5 +26,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created', 'updated', 'name',
-            'content', 'image', 'is_owner', 'following_id'
+            'content', 'image', 'is_owner', 'following_id', 'posts_count', 'followers_count', 'following_count'
         ]
