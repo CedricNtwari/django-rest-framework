b29345d6ec2c60d39ceb04baa5a44c2164d6d3e1
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-13 13:15:49 +0200

    Add Follower feature with views, models, and serializers

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
+    path('', include('followers.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
