1fec48a301491bdbe6654dd42da36ac5cec3d4ee
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-13 18:00:53 +0200

    Add Post filter

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
@@ -19,6 +20,12 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
     ]
     search_fields = [ 'owner__username', 'title']
 
