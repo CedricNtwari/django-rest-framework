9453b5a560b8ed77171a5dffcecceae830083976
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-17 11:37:58 +0200

    Fix ALLOWED_HOSTS configuration to handle multiple hosts correctly

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -70,11 +70,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Ensure CSRF Trusted Origins are set correctly
 CSRF_TRUSTED_ORIGINS = [
